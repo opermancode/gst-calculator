caacfa807335f264711815a50bcdf1f6624409ff
Omkar Jagtap <152702656+meomkarjagtap@users.noreply.github.com>
2026-05-03 18:00:21 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const USER_SERVICE = "http://localhost:8081";
-const GST_SERVICE = "http://localhost:8080";
+const EC2_IP = "http://13.218.219.188";   // ← Change to your actual EC2 Public IP
+
+const USER_SERVICE = `${EC2_IP}:8081`;
+const GST_SERVICE = `${EC2_IP}:8080`;
+
+console.log("Using API:", USER_SERVICE); // for debugging
 
 // Signup
 export const signup = (data) => {
